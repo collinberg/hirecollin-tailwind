9de29f55b81453b7b66b717ff16352fce4e29594
Collin B <berg.collin@gmail.com>
2020-04-19 19:47:55 -0400

    Added Search Dropdown

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,32 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    function searchToggle() {
+      console.log('toggleSearch');
+      const searchWrap =  document.querySelector('.search-wrap');
+
+      if( $('.search-modal').hasClass('search-is-active') ) {
+        $('.search-modal').removeClass('search-is-active');
+        searchWrap.classList.remove('animated', 'bounceInDown');
+
+        $('.close-search').toggleClass('hidden');
+        $('.search-icon').toggleClass('hidden');
+        $('body').toggleClass('overflow-hidden');
+      } else {
+
+        $('.search-modal').addClass('search-is-active');
+        searchWrap.classList.add('animated', 'bounceInDown');
+        $('.search-field').focus();
+
+        $('.search-icon').toggleClass('hidden');
+        $('.close-search').toggleClass('hidden');
+        $('body').toggleClass('overflow-hidden');
+      }
+
+
+    }
+
+    $('.search-toggle').click(searchToggle);
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
---
+++ resources/views/partials/searchform.blade.php
@@ -0,0 +1,9 @@
+<form role="search" method="get" class="search-form flex my-1" action="{{ esc_url( home_url( '/' ) ) }}">
+  <label class='w-full'>
+    <span class="screen-reader-text">{{ _x( 'Search for:', 'label' ) }}</span>
+    <input type="search" class="search-field text-3xl border-b-2 border-grey-darkest" placeholder="{!! esc_attr_x( 'Search &hellip;', 'placeholder' ) !!}" value="{{ get_search_query() }}" name="s" />
+  </label>
+  <button type="submit" class="search-submit" value="{{ esc_attr_x( 'Search ', 'submit button' ) }}" />
+    Search <i class='fas fa-search'></i>
+  </button>
+</form>
