c95f8ff71c45de2df700839636cab752a306ae1d
Collin B <berg.collin@gmail.com>
2020-04-21 14:14:16 -0400

    Added Mobile Search & Menu

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -2,31 +2,70 @@ export default {
   init() {
     // JavaScript to be fired on all pages
     function searchToggle() {
-      console.log('toggleSearch');
       const searchWrap =  document.querySelector('.search-wrap');
+      const searchModal =  document.querySelector('.search-modal');
+      let searchToggle = document.querySelector('#searchToggle');
 
-      if( $('.search-modal').hasClass('search-is-active') ) {
-        $('.search-modal').removeClass('search-is-active');
+      if( searchToggle.dataset.searchToggle == 'closed' ) {
+        //Close Search
+        searchToggle.dataset.searchToggle = 'open';
+
         searchWrap.classList.remove('animated', 'bounceInDown');
+        searchWrap.classList.add('animated', 'bounceOutUp');
 
-        $('.close-search').toggleClass('hidden');
-        $('.search-icon').toggleClass('hidden');
-        $('body').toggleClass('overflow-hidden');
+        searchModal.classList.add('animated', 'bounceOutUp');
       } else {
+        //Open Search
+        searchToggle.dataset.searchToggle = 'closed';
 
-        $('.search-modal').addClass('search-is-active');
+        searchModal.classList.remove('animated', 'bounceOutUp');
+        searchModal.classList.add('search-is-active');
+
+        searchWrap.classList.remove('animated', 'bounceOutUp');
         searchWrap.classList.add('animated', 'bounceInDown');
+
         $('.search-field').focus();
-
-        $('.search-icon').toggleClass('hidden');
-        $('.close-search').toggleClass('hidden');
-        $('body').toggleClass('overflow-hidden');
       }
-
+      //Icon Change
+      $('.close-search').toggleClass('hidden');
+      $('.search-icon').toggleClass('hidden');
+      //Body Change
+      $('body').toggleClass('overflow-hidden');
 
     }
 
-    $('.search-toggle').click(searchToggle);
+    let menuToggle =  document.querySelector('#menuToggle');
+
+    function mobileMenuToggle() {
+      let mobileMenu = document.querySelector('#mobileMenu');
+      let menuToggle = document.querySelector('#menuToggle');
+
+      if( menuToggle.dataset.menuState == 'closed' ) {
+        console.log('Opening Menu');
+
+
+        //icon
+        menuToggle.dataset.menuState = 'open';
+        menuToggle.classList.toggle('open');
+
+        //Menu
+        mobileMenu.classList.remove('open','bounceOutRight');
+        mobileMenu.classList.add('open','bounceInRight');
+
+      } else {
+        console.log('Closing Menu');
+        //icon
+        menuToggle.dataset.menuState = 'closed';
+        menuToggle.classList.toggle('open');
+
+        mobileMenu.classList.remove('bounceInRight');
+        mobileMenu.classList.add('bounceOutRight');
+      }
+    }
+
+    $('#searchToggle').click(searchToggle);
+
+    menuToggle.addEventListener('click', mobileMenuToggle)
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
