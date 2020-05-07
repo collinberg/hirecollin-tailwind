516a956071a21b066dd93d019062221c5a10d1a1
Collin B <berg.collin@gmail.com>
2020-05-07 13:50:39 -0400

    removed rounded corners

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -1,13 +1,13 @@
-<div class='w-full md:w-1/3 px-3 lg:p-6 flex flex-col flex-grow flex-shrink transform hover:scale-105 transition duration-500 ease-in-out hover:text-primary'>
-  <div class='flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg'>
+<div class='w-full md:w-1/3 px-3 lg:p-6 flex flex-col flex-grow flex-shrink transform hover:scale-105 transition duration-500 ease-in-out hover:text-primary mb-5'>
+  <div class='flex-1 bg-white overflow-hidden shadow-lg'>
     <a href="{!! $home_item['permalink'] !!}" title="" class='flex flex-wrap no-underline hover:no-underline border-none'>
-      <img src="{!! $home_item['thumbnail'] !!}" class="h-full w-full rounded-t pb-6">
-      <span class='w-full text-xs md:text-sm px-6 uppercase mb-2'><i class="fas fa-clock"></i> {!! $home_item['date'] !!}</span>
+      <img src="{!! $home_item['thumbnail'] !!}" class="h-full w-full pb-6">
+      <span class='w-full text-xs md:text-sm px-6 uppercase mb-2 font-bold'>{!! $home_item['date'] !!}</span>
       <h3 class='w-full font-bold text-xl px-6 text-black hover:text-primary'>{!! $home_item['title'] !!}</h3>
-      <span class='text-base px-6 mb-5 text-grey-darkest leading-normal'>{!! $home_item['content'] !!}</span>
+      <span class='text-base px-6 mb-5 text-grey-darkest leading-normal md:block hidden'>{!! $home_item['content'] !!}</span>
     </a>
   </div>
-  <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg px-6 py-3">
+  <div class="flex-none mt-auto bg-white overflow-hidden shadow-lg px-6 py-3">
     <div class='flex items-center justify-between'>
       @foreach($home_item['categories'] as $cat)
       @php $homecat = get_category($cat) @endphp
