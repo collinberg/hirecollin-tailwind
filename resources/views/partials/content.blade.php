bdcd0a735006f09492899960ed2abc3365fd73b5
collinberg <berg.collin@gmail.com>
2021-02-22 13:25:28 -0500

    re-arranged order

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,12 +1,9 @@
-<article @php post_class('flex flex-wrap mb-10') @endphp>
-  <div class=''>
-    <header>
-      <h2 class="entry-title text-4xl font-display font-bold"><a href="{{ get_permalink() }}" class='text-black hover:text-primary border-none hover:border-none'>{!! get_the_title() !!}</a></h2>
-      @include('partials/entry-meta')
-
-    </header>
-    <div class="entry-summary lg:text-lg">
-      @php the_excerpt() @endphp
-    </div>
+<article @php post_class('flex flex-wrap mb-5 pb-5 border-b-2') @endphp>
+  <header class='w-full'>
+    @include('partials/entry-meta')
+    <h2 class="entry-title text-4xl font-display font-bold lg:w-4/5"><a href="{{ get_permalink() }}" class='text-black hover:text-primary border-none hover:border-none'>{!! get_the_title() !!}</a></h2>
+  </header>
+  <div class="entry-summary lg:text-lg">
+    @php the_excerpt() @endphp
   </div>
 </article>
