81430230c864d4af870a1ee3926e5ae3b97af85b
collinberg <berg.collin@gmail.com>
2021-03-14 11:48:11 -0400

    search result page and 404 page fixes

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
   @include('partials.page-header')
-
+<section class='container pt-5 lg:pt-10 flex flex-wrap h-screen'>
   @if (!have_posts())
     <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+      {{ __('Sorry, I can\'t seem to find anything on that. ¯\_(ツ)_/¯ ' , 'sage') }}
     </div>
     {!! get_search_form(false) !!}
   @endif
+</section>
 @endsection
--- resources/views/partials/content-search.blade.php
+++ resources/views/partials/content-search.blade.php
@@ -1,11 +1,11 @@
-<article @php post_class() @endphp>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
+<article @php post_class('flex flex-wrap mb-5 pb-5 border-b-2 dark:border-grey-darkest') @endphp>
+  <header class='w-full'>
     @if (get_post_type() === 'post')
       @include('partials/entry-meta')
     @endif
+    <h2 class="entry-title text-2xl md:text-4xl font-display font-bold lg:w-4/5"><a href="{{ get_permalink() }}" class='text-black dark:text-grey-lighter hover:text-primary border-none hover:border-none'>{!! get_the_title() !!}</a></h2>
   </header>
-  <div class="entry-summary">
+  <div class="entry-summary lg:text-lg">
     @php the_excerpt() @endphp
   </div>
 </article>
